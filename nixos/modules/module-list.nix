8c7b60d9e5d643a9a368b8e22cde85b39f13bc17
nikstur <nikstur@outlook.com>
2023-10-17 02:21:44 +0200

    nixos/sysusers: factor into separate module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1424,6 +1424,7 @@
   ./system/boot/systemd/repart.nix
   ./system/boot/systemd/shutdown.nix
   ./system/boot/systemd/sysupdate.nix
+  ./system/boot/systemd/sysusers.nix
   ./system/boot/systemd/tmpfiles.nix
   ./system/boot/systemd/user.nix
   ./system/boot/systemd/userdbd.nix
